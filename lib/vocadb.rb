c4e7d1e81c194dc8eb9df4b66261d117de9a75ae
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-06-05 07:18:47 +0900

    Initial commit

---
+++ lib/vocadb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "vocadb/version"
+
+module Vocadb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/vocadb/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Vocadb
+  VERSION = "0.1.0"
+end
